a483b7813ec6d160c0fc0f32a3bff598e1b886cd
Dimas <faizanto27@gmail.com>
2026-01-20 12:21:37 +0700

    Delete app.py

--- app.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title="Dashboard Saham", layout="wide")
-
-st.title("📊 Dashboard Kesehatan Saham IDX")
-
-st.info("Repo dan Streamlit sudah terhubung. Langkah berikutnya: tarik data & visualisasi.")
